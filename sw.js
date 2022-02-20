266943f7130077327f2e1d0517ff908557f50ea7
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-02-20 21:46:53 +0800

    Cache SVG favicon

--- sw.js
+++ sw.js
@@ -6,7 +6,8 @@ var urlsToCache = [
   'favicon.svg',
   'manifest.webmanifest',
   'average-color.js',
-  'icon-192x192.png'
+  'icon-192x192.png',
+  'favicon.svg'
 ];
 
 self.addEventListener('install', function(event) {
